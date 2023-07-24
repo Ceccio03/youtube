02bb20afaed92b43955cec8a72f1dde960885e7f
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-24 10:48:56 +0200

    commit

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const player = document.querySelector('.video-stream');
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
 
-function getTime() {
-    console.log(player.getCurrentTime());
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+    const start = parseFloat(localStorage.getItem('video-time'));
+
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: 'PkZNo7MFNFg'
+    });
+}
+function goToSecondPage() {
+    localStorage.setItem('video-time', player.getCurrentTime() + '');
+    window.location = './second.html';
 }
---
+++ second.js
@@ -0,0 +1,21 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+    const start = parseFloat(localStorage.getItem('video-time'));
+    
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: 'PkZNo7MFNFg',
+        playerVars : {
+            start: start,
+            autoplay: 1
+        }
+    });
+}
